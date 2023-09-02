c12123ab51ea03d024e612d3fe17e64354deb6d4
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2023-09-02 10:40:51 +0900

    Update: Rails側にReactに対応したルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root to: redirect('/events')
+
+  get 'events', to: 'home#index'
+  get 'events/new', to: 'home#index'
+  get 'events/:id', to: 'home#index'
+  get 'events/:id/edit', to: 'home#index'
 
   namespace :api do
     resources :events, only: %i[index show create update destroy]
