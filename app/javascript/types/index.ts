89682ea360c40d7b4dd12238011757088ff028b5
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2023-09-02 17:41:38 +0900

    Fix: publishedの型が誤っていたので修正

--- app/javascript/types/index.ts
+++ app/javascript/types/index.ts
@@ -1,11 +1,11 @@
 export type EventParams = {
-  id: number;
+  id?: number;
   type: string;
   date: string;
   title: string;
   speaker: string;
   host: string;
-  published: string;
+  published: boolean;
 };
 
 export type EventListProps = {
