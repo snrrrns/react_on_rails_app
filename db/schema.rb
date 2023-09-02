cec0bcbf6b3c2ad330bc61baf273fe9b0a1eb8fa
suguru nakagawa <s.nakagawa.n4s3@gmail.com>
2023-09-02 09:30:34 +0900

    Fix: publishedにNouNull制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_27_084746) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_002533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_27_084746) do
     t.text "title"
     t.string "speaker"
     t.string "host"
-    t.boolean "published"
+    t.boolean "published", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
